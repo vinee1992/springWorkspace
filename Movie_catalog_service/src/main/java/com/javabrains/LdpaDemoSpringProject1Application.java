368ae0eca847f4605f17d4c395022805e908f68d
Vineet Chaurasia <vineetchaurasia2010@gmail.com>
2019-04-27 13:20:42 +0530

    Microservice Demo
    3 projects (services) :
    Movie_catalog_service,Rating_info_data,Movie_info_data

---
+++ Movie_catalog_service/src/main/java/com/javabrains/LdpaDemoSpringProject1Application.java
@@ -0,0 +1,13 @@
+package com.javabrains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdpaDemoSpringProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdpaDemoSpringProject1Application.class, args);
+	}
+
+}
